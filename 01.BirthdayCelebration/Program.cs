59cffda1436dd45650f1784574921107fe51b9d7
DimitarStoimenov <stoimenovdimitur@yahoo.com>
2022-06-22 19:46:54 +0200

    C#

---
+++ 01.BirthdayCelebration/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] eatingCapacityOfAPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] plates = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> capacityOfPerson = new Queue<int>(eatingCapacityOfAPerson);
+            Stack<int> stackPlates = new Stack<int>(plates);
+            int wastedFood = 0;
+           
+
+            while (capacityOfPerson.Count > 0 && stackPlates.Count > 0)
+            {
+                if (capacityOfPerson.Peek() <= stackPlates.Peek())
+                {
+                   
+                    int currCapacityPerson =   stackPlates.Peek() - capacityOfPerson.Peek();
+                   
+                    capacityOfPerson.Dequeue();
+                    stackPlates.Pop();
+                    
+                    wastedFood += currCapacityPerson;
+                }
+               else if (capacityOfPerson.Peek() > stackPlates.Peek())
+                {
+                   
+                    int currPerson = capacityOfPerson.Peek();
+                    
+
+                    while (currPerson > 0)
+
+                    {
+
+                        if (currPerson < stackPlates.Peek())
+                        {
+                            wastedFood += stackPlates.Peek() - currPerson;
+                            currPerson = 0;
+                            capacityOfPerson.Dequeue();
+                            stackPlates.Pop();
+                            break;
+                        }
+                        int curr = currPerson - stackPlates.Pop();
+                        if (curr <= 0)
+                        {
+                            
+                            capacityOfPerson.Dequeue();
+                            wastedFood += curr;
+                        }
+
+                        currPerson = curr;
+                        
+                      
+
+
+                    }
+
+                }
+
+            }
+
+            if (capacityOfPerson.Count == 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", stackPlates)}");
+               
+                Console.WriteLine($"Wasted grams of food: {Math.Abs(wastedFood)}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Guests: {string.Join(" ", capacityOfPerson)}");
+               
+                Console.WriteLine($"Wasted grams of food: {Math.Abs(wastedFood)}");
+            }
+
+
+        }
+    }
+}
---
+++ 02.The Battle of The Five Armies/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace The_Battle_of_The_Five_Armies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armory = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+            int armRow = 0;
+            int armCol = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+
+                matrix[i] = input;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 'A')
+                    {
+                        armRow = i;
+                        armCol = j;
+                        break;
+                    }
+                }
+            }
+
+            
+            while (true)
+            {
+                string commandLine = Console.ReadLine();
+                string command = commandLine.Split()[0];
+                int orcRow = int.Parse(commandLine.Split()[1]);
+                int orcCol = int.Parse(commandLine.Split()[2]);
+                matrix[orcRow][orcCol] = 'O';
+                armory--;
+                matrix[armRow][armCol] = '-';
+                if (command == "up" && armRow -1 >= 0)
+                {
+                    armRow--;
+                }
+                else if (command == "down" && armRow + 1 < n )
+                {
+                    armRow++;
+                }
+                else if (command == "left" && armCol -1 >= 0 )
+                {
+                    armCol--;
+                }
+                else if (command == "right" && armCol + 1 < matrix[armRow].Length)
+                {
+                    armCol++;
+                }
+                if (matrix[armRow][armCol] == 'O')
+                {
+                    matrix[armRow][armCol] = '-';
+                    armory -= 2;
+                }
+                if (matrix[armRow][armCol] == 'M')
+                {
+                    matrix[armRow][armCol] = '-';
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armory}");
+                    break;
+                }
+                if (armory <= 0)
+                {
+                    matrix[armRow][armCol] = 'X';
+                    Console.WriteLine($"The army was defeated at {armRow};{armCol}.");
+                    break;
+                }
+
+                matrix[armRow][armCol] = 'A';
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(matrix[i]);
+            }
+        }
+    }
+}
